8686bd19515b185f516956024147027b6fa63fe5
jacob thornton <Jet2002Jake@gmail.com>
2018-09-11 09:33:59 +0100

    changed hello to goodbye

--- hello/Form1.cs
+++ hello/Form1.cs
@@ -19,7 +19,7 @@ namespace hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("hello " + txtbxname.Text);
+            MessageBox.Show("goodbye " + txtbxname.Text);
 
         }
     }
